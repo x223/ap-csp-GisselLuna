376d478319e2cc436517beec6ee1a65ae054198a
GisselLuna <gissel.luna@ms223.org>
2016-09-29 14:29:10 -0400

    Add files via upload

---
+++ bounceball/sketch.js
@@ -0,0 +1,22 @@
+var X= 1;
+var Y= 1;
+var xpos =1;
+var ypos = 1;
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+  background('skyblue');
+  ellipse(60,60,60,60);
+
+  
+  
+if (xpos > windowWidth) {
+  xdir=xdir * -1;
+  
+} else { 
+  
+  
+}
+}
