8db7d442ae9290b4ba48d33416ece32dbf12bb5c
GisselLuna <gissel.luna@ms223.org>
2016-10-14 14:29:22 -0400

    Add files via upload

---
+++ Traffic/sketch.js
@@ -0,0 +1,21 @@
+var fillcolor = 'red'
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(0);
+  fill('pink');
+  rect(110,90,120,230);
+  fill('white')	
+  ellipse(170,150,50,50);
+  ellipse(170,210,50,50);
+  ellipse(170,270,50,50);
+  
+if (mouseX > = windowwidth /2) {
+  fillcolor = ('red')
+  
+  
+}
+}
